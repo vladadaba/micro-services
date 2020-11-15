755a2ad1417618ae0329346fc301a9d4c83bd2a7
Vladimir Dabic <vladadaba92@gmail.com>
2020-11-15 10:18:44 +0100

    Add CreditService with dummy controller

---
+++ src/services/CreditService/Controllers/CreditController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobService.Controllers
+{
+    [ApiController]
+    [Route("api/me")]
+    [Produces("application/json")]
+    [Consumes("application/json")]
+    public class CreditController : ControllerBase
+    {
+        public CreditController()
+        {
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<string> Get()
+        {
+            return "dummy";
+        }
+    }
+}
